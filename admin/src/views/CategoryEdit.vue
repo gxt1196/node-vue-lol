ea516b691454123558ef917779d9cfdd2d762733
gxt1196 <1448271564@qq.com>
2019-12-02 01:49:19 +0800

    初次修改1202

---
+++ admin/src/views/CategoryEdit.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="about">
+        <h1>新建分类</h1>
+        <el-form>
+            <el-form-item label="名称">
+                <el-input></el-input>
+            </el-form-item></el-form>
+    </div>
+</template>
